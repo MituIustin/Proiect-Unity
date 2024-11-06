766480852aa4c7bad49aebeb2597ac94f80f8988
MituIustin <126506678+MituIustin@users.noreply.github.com>
2024-11-06 23:55:45 +0200

    final change raport 3

--- Assets/Scripts/IntroLevel.cs
+++ Assets/Scripts/IntroLevel.cs
@@ -5,15 +5,17 @@ public class IntroLevel : MonoBehaviour
     public GameObject playerPrefab;
     public GameObject enemy1Prefab;
     public GameObject enemy2Prefab;
+    public GameObject camera;
     GameObject player;
     public GameObject UiPrefab;
     GameObject UI;
     void Start()
     {
-        player= Instantiate(playerPrefab);
+       // player= Instantiate(playerPrefab);
         Instantiate(enemy1Prefab);
         Instantiate(enemy2Prefab);
-        UI =Instantiate(UiPrefab);
+       // Instantiate(camera);
+        UI = Instantiate(UiPrefab);
     }
 
     void Update()
