2874e5e358ece059d710a4609e1d9cc7a98857bb
Dan Dragos <dragos-andrei.dan@s.unibuc.ro>
2024-11-13 22:34:19 +0200

    Created NPC Dialogue + Camera + Sprite Shader)
    Created NPC Dialogue + Camera + World Boundaries + Shader for sprites (casts shadows and accepts lightning)

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    public GameObject dialogueBox;
+    public Text dialogueText;
+
+    public string[] dialogue;
+
+    public static bool isDialogueActive;
+
+    private int index;
+
+    public float wordspeed;
+
+    public bool playerInRange;
+
+    public Text interactText; 
+
+    void Start()
+    {
+        dialogueBox.SetActive(false);
+        isDialogueActive = false;
+
+     
+        if (interactText != null)
+        {
+            interactText.gameObject.SetActive(false);
+        }
+    }
+
+   
+    void Update()
+    {
+        if (playerInRange && !isDialogueActive) 
+        {
+            if (interactText != null && !interactText.gameObject.activeSelf)
+            {
+                interactText.gameObject.SetActive(true); 
+                interactText.text = "Press E to interact"; 
+            }
+        }
+        else if (!playerInRange || isDialogueActive) 
+        {
+            if (interactText != null && interactText.gameObject.activeSelf)
+            {
+                interactText.gameObject.SetActive(false); 
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange)
+        {
+            if (dialogueBox.activeInHierarchy)
+            {
+           
+                if (dialogueText.text == dialogue[index])
+                {
+                    NextLine();
+                }
+            }
+            else
+            {
+                dialogueBox.SetActive(true);
+                StartCoroutine(TypingText());
+                isDialogueActive = true;
+            }
+        }
+    }
+
+    public void ZeroText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialogueBox.SetActive(false);
+    }
+
+    IEnumerator TypingText()
+    {
+        dialogueText.text = "";
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordspeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            StartCoroutine(TypingText());
+        }
+        else
+        {
+            ZeroText();
+            isDialogueActive = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            ZeroText();
+            isDialogueActive = false;
+        }
+    }
+}
